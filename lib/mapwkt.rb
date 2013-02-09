1afb9472a3f5d501c68a1ea62429ca346044d5b4
Jesse Sielaff <jesse.sielaff@gmail.com>
2013-02-09 10:21:06 -0500

    Added files for Map and WKT types.

--- lib/mapwkt.rb
+++ lib/mapwkt.rb
@@ -1,4 +1,14 @@
 # encoding: UTF-8
 
-require 'bundler/setup'
-require 'active_support/all'
+module MapWKT
+  require 'bundler/setup'
+  require 'active_support/all'
+  
+  require_relative 'mapwkt/line_string'
+  require_relative 'mapwkt/map'
+  require_relative 'mapwkt/multi_line_string'
+  require_relative 'mapwkt/multi_point'
+  require_relative 'mapwkt/multi_polygon'
+  require_relative 'mapwkt/point'
+  require_relative 'mapwkt/polygon'
+end
